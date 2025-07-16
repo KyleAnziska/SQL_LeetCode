232e6491b0cd9621ebd6a83cd7e26972a6cc0311
Kyle Anziska <83586303+KyleAnziska@users.noreply.github.com>
2025-07-16 20:09:49 +0200

    Time: 259 ms (76.75%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select name, population, area from World where area >= 3000000 or population >= 25000000;
+select name, population, area
+from ( select name, population, area,
+    case 
+        when area >= 3000000 then true
+        when population >= 25000000 then true 
+        else false
+        end as is_bigger from world) has_big
+where is_bigger >0
